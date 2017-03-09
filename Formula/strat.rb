8c2a70f047b82ed56f3d92d8eab2d0e84a2f66cb
Stephan Florquin <stephan.florquin@gmail.com>
2017-03-09 17:16:24 +0100

    fix

--- Formula/strat.rb
+++ Formula/strat.rb
@@ -1,7 +1,7 @@
 class Strat < Formula
   desc "Stratumn's SDK to create Indigo applications and networks"
   homepage "https://stratumn.com"
-  url "https://github.com/stratumn/sdk/releases/download/v0.0.7-dev/strat-darwin-amd64.zip", :using => :curl
+  url "https://github.com/stratumn/sdk/releases/download/v0.0.7-dev/strat-darwin-amd64.zip"
   version "v0.0.7-dev"
   sha256 "d6f38a303efe220ea38522a3e1204ef9f3e3e2cdee439f1fb9405713917866b9"
 
