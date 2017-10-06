885dc316ddf409d91a1d9f81871793212b90f8b7
Stephan Florquin <stephan.florquin@gmail.com>
2017-10-06 16:55:13 +0200

    strat: fix sha

--- Formula/strat.rb
+++ Formula/strat.rb
@@ -3,7 +3,7 @@ class Strat < Formula
   homepage "https://stratumn.com"
   url "https://github.com/stratumn/sdk/releases/download/v0.1.0/strat-darwin-amd64.zip"
   version "v0.1.0"
-  sha256 " 47168ef3e77fee813807572837a58d3349ac58bf68ee210df7b7acd072fdae10"
+  sha256 "47168ef3e77fee813807572837a58d3349ac58bf68ee210df7b7acd072fdae10"
 
   bottle :unneeded
 
